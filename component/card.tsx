de52d85957165a7ad3bc9ceb4850d8773722edef
Richard Lock <lock.j.h@gmail.com>
2022-12-06 18:14:42 -0500

    chore: remove legacy anchor element

--- component/card.tsx
+++ component/card.tsx
@@ -1,29 +1,24 @@
-
-
 import Link from "next/link";
 import Image from "next/image";
-
 import styles from "./card.module.css";
 
-const Card = (props:any) => {
+const Card = (props: any) => {
   return (
-    <Link href={props.href}>
-      <a className={styles.cardLink}>
-        <div className={`glass ${styles.container}`}>
-          <div className={styles.cardHeaderWrapper}>
-            <h2 className={styles.cardHeader}>{props.name}</h2>
-          </div>
-          <div className={styles.cardImageWrapper}>
-            <Image
-              className={styles.cardImage}
-              src={props.imgUrl}
-              width={260}
-              height={160}
-              alt={props.name}
-            />
-          </div>
+    <Link href={props.href} className={styles.cardLink}>
+      <div className={`glass ${styles.container}`}>
+        <div className={styles.cardHeaderWrapper}>
+          <h2 className={styles.cardHeader}>{props.name}</h2>
         </div>
-      </a>
+        <div className={styles.cardImageWrapper}>
+          <Image
+            className={styles.cardImage}
+            src={props.imgUrl}
+            width={260}
+            height={160}
+            alt={props.name}
+          />
+        </div>
+      </div>
     </Link>
   );
 };
